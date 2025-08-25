7b318aa156b3352e8c8c1a088c41e227eb5c4280
Guilh3rmeParteka <guilherme.parteka@escola.pr.gov.br>
2025-08-25 13:56:03 -0300

    começo

---
+++ script.js
@@ -0,0 +1,40 @@
+function responder(estilo) {
+  let jogador = {};
+  
+  switch (estilo) {
+    case 'ataque':
+      jogador = {
+        nome: "Vinícius Júnior",
+        imagem: "img/vini.jpg",
+        descricao: "Você é habilidoso, rápido e imprevisível no ataque!"
+      };
+      break;
+    case 'meio':
+      jogador = {
+        nome: "Jude Bellingham",
+        imagem: "img/bellingham.jpg",
+        descricao: "Você controla o jogo com classe e inteligência!"
+      };
+      break;
+    case 'defesa':
+      jogador = {
+        nome: "Éder Militão",
+        imagem: "img/militao.jpg",
+        descricao: "Você é o muro da defesa, sempre confiável."
+      };
+      break;
+    case 'goleiro':
+      jogador = {
+        nome: "Thibaut Courtois",
+        imagem: "img/courtois.jpg",
+        descricao: "Você é o último obstáculo. Calmo, estratégico e seguro!"
+      };
+      break;
+  }
+
+  document.getElementById('quiz').classList.add('hidden');
+  document.getElementById('resultado').classList.remove('hidden');
+  document.getElementById('jogador-nome').textContent = jogador.nome;
+  document.getElementById('jogador-img').src = jogador.imagem;
+  document.getElementById('jogador-desc').textContent = jogador.descricao;
+}
